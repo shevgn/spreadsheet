8d3012b148aefe0dbd259c4e2121886b8900324b
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2025-07-31 23:15:07 +0300

    layout with sheet context

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,79 @@
 <script lang="ts">
-	import '../app.css';
+    import { createSheetContext } from '$lib/SheetContext.svelte';
+    import type { Cell } from '$lib/types';
+    import '../app.css';
 
-	let { children } = $props();
+    let { children } = $props();
+
+    let cells = $state<Cell[][]>([
+        [
+            {
+                value: '1',
+                bgColor: 'red',
+                color: 'white'
+            },
+            {
+                value: '2',
+                bgColor: 'green',
+                color: 'white'
+            },
+            {
+                value: '3',
+                bgColor: 'blue',
+                color: 'white'
+            }
+        ],
+        [
+            {
+                value: '4',
+                bgColor: 'red',
+                color: 'white'
+            },
+            {
+                value: '5',
+                bgColor: 'green',
+                color: 'white'
+            },
+            {
+                value: '6',
+                bgColor: 'blue',
+                color: 'white'
+            }
+        ],
+        [
+            {
+                value: '7',
+                bgColor: 'red',
+                color: 'white'
+            },
+            {
+                value: '8',
+                bgColor: 'green',
+                color: 'white'
+            },
+            {
+                value: '9',
+                bgColor: 'blue',
+                color: 'white'
+            }
+        ]
+    ]);
+
+    createSheetContext(cells);
 </script>
 
-{@render children()}
+<div class="flex min-h-screen flex-col">
+    <header class="flex min-h-14 items-center justify-between">
+        <div class="container mx-auto py-4">
+            <h1 class="text-3xl font-bold">Spreadsheet</h1>
+        </div>
+    </header>
+    <main class="flex flex-1 items-center justify-center p-4">
+        {@render children()}
+    </main>
+    <footer class="flex min-h-14 items-center justify-between">
+        <div class="container mx-auto py-4">
+            <p>&copy; 2025 Levchenko Artem</p>
+        </div>
+    </footer>
+</div>
