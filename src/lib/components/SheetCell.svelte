836437558af465ad9360dbea062e1505a5d41e8f
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2025-08-14 23:53:45 +0300

    select cell for highlight

--- src/lib/components/SheetCell.svelte
+++ src/lib/components/SheetCell.svelte
@@ -5,94 +5,116 @@
     type Props = {
         col: number;
         row: number;
+        selected?: boolean;
+        selectCell: (row: number, col: number) => void;
         width?: number;
         height?: number;
     };
 
-    let { col, row, width, height }: Props = $props();
+    let { col, row, selected, selectCell, width, height }: Props = $props();
 
     const DEFAULT_WIDTH = 80;
-    const DEFAULT_HEIGHT = 40;
+    const DEFAULT_HEIGHT = 30;
 
-    if (!width) {
-        width = DEFAULT_WIDTH;
-    }
-    if (!height) {
-        height = DEFAULT_HEIGHT;
-    }
+    if (!width) width = DEFAULT_WIDTH;
+    if (!height) height = DEFAULT_HEIGHT;
 
     let sheetContext = getSheetContext();
 
     let isAlphabetHeader = $state(col > 0 && row === 0);
     let isNumberHeader = $state(col === 0 && row > 0);
-
     let isEditing = $state(false);
+    let isHeader = $derived(col == 0 || row == 0);
+    let isResizing = $state(false);
 
     const focusOnMount = (element: HTMLInputElement) => {
         element.focus();
     };
-
-    let isHeader = $derived(col == 0 || row == 0);
 </script>
 
 <svelte:element
     this={isHeader ? 'th' : 'td'}
-    class="border"
-    style={`width: ${width}px; 
-            height: ${height}px; 
-            background-color: ${isHeader ? '#f7f7f7' : '#ffffff'}
-    `}
+    role="cell"
+    tabindex="0"
+    class={[
+        'relative text-sm',
+        !isHeader && !selected && 'border-[0.5px]',
+        selected && 'border-blue-500'
+    ]}
+    class:border={isHeader}
+    class:border-2={selected}
+    onclick={() => {
+        if (row === 0 && col === 0) {
+            return;
+        }
+        selectCell(row, col);
+    }}
+    ondblclick={() => {
+        isEditing = true;
+    }}
+    style={`width: ${width}px;height: ${height}px;background-color: ${isHeader ? '#f7f7f7' : '#ffffff'}`}
 >
-    <div class="flex h-full items-center justify-center">
+    {#if isAlphabetHeader}
+        <div
+            id={'cell-' + row + '-' + col + '-resize-left-handle'}
+            role="slider"
+            aria-valuenow={width}
+            tabindex="0"
+            class="absolute top-0 bottom-0 left-0 w-1 cursor-col-resize hover:bg-green-400"
+        ></div>
+        <div
+            id={'cell-' + row + '-' + col + '-resize-right-handle'}
+            role="slider"
+            aria-valuenow={width}
+            tabindex="0"
+            class="absolute top-0 right-0 bottom-0 w-1 cursor-col-resize hover:bg-green-400"
+        ></div>
+    {/if}
+    <div class="flex h-full w-full items-center justify-center">
         {#if isAlphabetHeader}
-            {numberToAlphabet(col - 1)}
+            <span class="pointer-events-none select-none">
+                {numberToAlphabet(col - 1)}
+            </span>
         {/if}
         {#if isNumberHeader}
-            {row}
+            <span class="pointer-events-none select-none">
+                {row}
+            </span>
         {/if}
         {#if col > 0 && row > 0}
             {#if isEditing}
-                <input
-                    type="text"
-                    name={'cell-' + row + '-' + col}
-                    class="h-full w-full"
-                    bind:value={
-                        () => sheetContext.safeCell(row - 1, col - 1).value,
-                        (v) =>
-                            sheetContext.updateCell(row - 1, col - 1, {
-                                ...sheetContext.safeCell(row - 1, col - 1),
-                                value: v
-                            })
-                    }
-                    use:focusOnMount
-                    onfocusout={() => {
-                        isEditing = false;
-                    }}
-                    onkeydown={(e) => {
-                        if (e.key === 'Enter' || e.key === 'Escape') {
-                            isEditing = false;
-                        }
-                    }}
-                />
+                {@render input()}
             {:else}
-                <button
-                    type="button"
-                    ondblclick={() => {
-                        isEditing = true;
-                    }}
-                    class="flex h-full flex-1 cursor-pointer items-center justify-center"
-                >
-                    {#if sheetContext.cells[row - 1]?.[col - 1]?.value}
-                        <p>
-                            {sheetContext.cells[row - 1][col - 1].value}
-                        </p>
-                    {:else}
-                        <span class="invisible text-gray-400/50">
-                            {numberToAlphabet(col - 1)}
-                        </span>
-                    {/if}
-                </button>
+                <p class="w-full text-center">
+                    {sheetContext.cells[row - 1]?.[col - 1]?.value ?? ''}
+                </p>
             {/if}
         {/if}
     </div>
 </svelte:element>
+
+{#snippet input()}
+    <input
+        type="text"
+        name={'cell-' + row + '-' + col}
+        class="absolute z-10 bg-white"
+        style:width={`${() => sheetContext.safeCell(row - 1, col - 1).value.length * 10}px`}
+        bind:value={
+            () => sheetContext.safeCell(row - 1, col - 1).value,
+            (v) =>
+                sheetContext.updateCell(row - 1, col - 1, {
+                    ...sheetContext.safeCell(row - 1, col - 1),
+                    value: v
+                })
+        }
+        use:focusOnMount
+        onfocusout={() => {
+            isEditing = false;
+        }}
+        onkeydown={(e) => {
+            if (e.key === 'Enter' || e.key === 'Escape') {
+                isEditing = false;
+            }
+        }}
+    />
+{/snippet}
