31d30ed596e3060c4dc8ad2bf30632e69d2be546
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2025-08-14 23:54:31 +0300

    test block

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,10 @@
     import Sheet from '$lib/components/Sheet.svelte';
 </script>
 
+<div class="flex h-20 w-full items-center p-4">
+    <div class="flex h-full flex-1 items-center rounded-2xl bg-zinc-100 px-4">
+        <p>Hello world!</p>
+    </div>
+</div>
+
 <Sheet />
