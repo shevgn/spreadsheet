cbc89c4efa8e1d6ba27ff1784649db28e1509702
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2025-08-16 18:45:29 +0300

    indicator for not saved changes

---
+++ src/lib/components/header/NotSavedIndicator.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import { fade } from 'svelte/transition';
+
+    type Props = {
+        isSaved: boolean;
+        isSaving: boolean;
+    };
+
+    let { isSaved, isSaving }: Props = $props();
+</script>
+
+{#if !isSaved && !isSaving}
+    <div
+        transition:fade={{ duration: 200 }}
+        class="flex items-center rounded-md border-2 border-red-100 bg-gradient-to-br from-red-200 to-red-50 px-3 py-1"
+        aria-live="polite"
+    >
+        <span class="font-medium text-red-800">Not saved</span>
+    </div>
+{/if}
