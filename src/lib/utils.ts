42caddbdf52c1fc6d6c382db2f7ad96af01f3d46
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2025-08-17 17:47:27 +0300

    clamp func

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,7 @@
 export function numberToAlphabet(num: number): string {
     return String.fromCharCode(num + 65);
 }
+
+export function clamp(v: number, a: number, b: number) {
+    return Math.max(a, Math.min(b, v));
+}
