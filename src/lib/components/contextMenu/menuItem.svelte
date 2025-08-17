555dd972483be2a20ef0829620477f2f0052a542
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2025-08-17 22:00:32 +0300

    ...rest props added

--- src/lib/components/contextMenu/menuItem.svelte
+++ src/lib/components/contextMenu/menuItem.svelte
@@ -7,7 +7,7 @@
         onClick?: () => void;
     } & HTMLButtonAttributes;
 
-    let { onClick, children }: Props = $props();
+    let { onClick, children, ...rest }: Props = $props();
 
     let context = contextMenuContext.get();
 
@@ -18,7 +18,8 @@
 
 <li>
     <button
-        class="w-full rounded px-2 py-1 text-left hover:bg-zinc-100"
+        {...rest}
+        class="w-full rounded px-2 py-1 text-left not-disabled:hover:bg-zinc-100 disabled:bg-zinc-100"
         data-action="close"
         onclick={() => {
             onClick?.();
