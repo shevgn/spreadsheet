d4b631dca50b87e25d5b81485cd20f3544949520
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2025-08-18 22:14:48 +0300

    temp fix: letters when overflow

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,16 @@
+import { MIN_COLS } from './components/sheet/SheetContext.svelte';
+
 export function numberToAlphabet(num: number): string {
+    const SYMBOLS_SKIP = 6;
+    const CASE_DIFF = 32;
+
+    if (num >= MIN_COLS) {
+        return (
+            String.fromCharCode(num + 65 - CASE_DIFF + SYMBOLS_SKIP) +
+            String.fromCharCode(num + 65 - CASE_DIFF + SYMBOLS_SKIP)
+        );
+    }
+
     return String.fromCharCode(num + 65);
 }
 
