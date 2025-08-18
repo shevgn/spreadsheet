e49d2388bf00fee3bccc637d0fbdcd74c7bfbe30
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2025-08-18 22:18:03 +0300

    control panel

---
+++ src/lib/components/mainControlPanel/Button.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import type { Snippet } from 'svelte';
+
+    type Props = {
+        onClick?: () => void;
+        children?: Snippet;
+    };
+
+    let { onClick, children }: Props = $props();
+</script>
+
+<button
+    type="button"
+    class="cursor-pointer rounded-full p-2 not-disabled:hover:scale-105 not-disabled:hover:bg-white"
+    onclick={onClick}
+>
+    {@render children?.()}
+</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,82 @@
 <script lang="ts">
-    import Sheet from '$lib/components/Sheet.svelte';
+    import Button from '$lib/components/mainControlPanel/Button.svelte';
+    import Delimiter from '$lib/components/mainControlPanel/Delimiter.svelte';
+    import Sheet from '$lib/components/sheet/Sheet.svelte';
+    import { getSheetContext } from '$lib/components/sheet/SheetContext.svelte';
+    import {
+        ChevronsLeftRightEllipsis,
+        Columns2,
+        FoldHorizontal,
+        Plus,
+        Rows2,
+        UnfoldHorizontal
+    } from '@lucide/svelte';
+
+    let sheetContext = getSheetContext();
 </script>
 
-<div class="flex h-20 w-full items-center p-4">
-    <div class="flex h-full flex-1 items-center rounded-2xl bg-zinc-100 px-4">
-        <p>Hello world!</p>
+<svelte:head>
+    <title>Spreadsheet</title>
+    <meta
+        name="description"
+        content="Simple spreadsheet application"
+    />
+</svelte:head>
+
+<div class="flex h-20 w-full items-center bg-white px-4 pt-2 pb-4">
+    <div
+        class="flex h-full flex-1 items-center rounded-2xl bg-zinc-100 px-4 py-1"
+    >
+        <div class="flex items-center space-x-2 p-2">
+            <Button onClick={() => sheetContext.minimizeColumnsWidths()}>
+                <span class="sr-only">Minimize columns widths</span>
+                <FoldHorizontal size="20" />
+            </Button>
+            <Button onClick={() => sheetContext.maximizeColumnsWidths()}>
+                <span class="sr-only">Maximize columns widths</span>
+                <UnfoldHorizontal size="20" />
+            </Button>
+            <Button onClick={() => sheetContext.resetColumnsWidths()}>
+                <span class="sr-only">Reset columns widths</span>
+                <ChevronsLeftRightEllipsis size="20" />
+            </Button>
+        </div>
+        <Delimiter />
+
+        <div class="flex items-center space-x-2 p-2">
+            <Button onClick={() => sheetContext.addRow()}>
+                <span class="sr-only">Add row</span>
+
+                <div class="relative">
+                    <Rows2 size="20" />
+                    <Plus
+                        size="14"
+                        class="absolute -bottom-1 -left-1 rounded-full bg-zinc-100"
+                    />
+                </div>
+            </Button>
+            <Button onClick={() => sheetContext.addColumn()}>
+                <span class="sr-only">Add column</span>
+                <div class="relative">
+                    <Columns2 size="20" />
+                    <Plus
+                        size="14"
+                        class="absolute -bottom-1 -left-1 rounded-full bg-zinc-100"
+                    />
+                </div>
+            </Button>
+            <Button onClick={() => sheetContext.deleteColumn(1)}>
+                <span class="sr-only">Delete column</span>
+                <div class="relative">
+                    <Rows2 size="20" />
+                    <Plus
+                        size="14"
+                        class="absolute -bottom-1 -left-1 rounded-full bg-zinc-100"
+                    />
+                </div>
+            </Button>
+        </div>
+        <Delimiter />
     </div>
 </div>
 
